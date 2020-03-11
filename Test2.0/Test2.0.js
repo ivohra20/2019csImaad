0700d203bc16b36fca02e4002c5d81ecd66ddebf
ivohra20 <ivohra20@nscds.org>
2020-03-11 16:39:44 +0000

    test

--- Test2.0/Test2.0.js
+++ Test2.0/Test2.0.js
@@ -3,6 +3,30 @@
 $('.type1').click(
     function(){
         var myColor = $(this).css("background-color");
-        $('.bar').css('background-color','red', myColor);
+        $('.bar').css('background-color','darkorange', myColor);
+    }
+);
+$('.type2').click(
+    function(){
+        var myColor = $(this).css("background-color");
+        $('.bar').css('background-color','indigo', myColor);
+    }
+);
+$('.type3').click(
+    function(){
+        var myColor = $(this).css("background-color");
+        $('.bar').css('background-color','darkred', myColor);
+    }
+);
+$('.type4').click(
+    function(){
+        var myColor = $(this).css("background-color");
+        $('.bar').css('background-color','darkred', myColor);
+    }
+);
+$('.type5').click(
+    function(){
+        var myColor = $(this).css("background-color");
+        $('.bar').css('background-color','indigo', myColor);
     }
 );
